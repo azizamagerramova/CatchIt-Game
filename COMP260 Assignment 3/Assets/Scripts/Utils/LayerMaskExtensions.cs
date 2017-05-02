5fb52a14408176f7d297dd0fe0752d81c2abe720
Malcolm Ryan <malcolm.ryan@gmail.com>
2017-05-02 14:48:05 +1000

    Initial commit

---
+++ COMP260 Assignment 3/Assets/Scripts/Utils/LayerMaskExtensions.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class LayerMaskExtensions  {
+
+	public static bool Contains(this LayerMask layerMask, GameObject gameObject) {
+		return (layerMask.value & (1 << gameObject.layer)) != 0;
+	}
+}
