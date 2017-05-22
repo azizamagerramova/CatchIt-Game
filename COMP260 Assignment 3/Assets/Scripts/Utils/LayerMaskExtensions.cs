27311eba8acfc519d1065419060249ef7345c098
Malcolm Ryan <malcolm.ryan@gmail.com>
2017-05-22 11:21:46 +1000

    Updated to Unity 5.5. Added some utility extension classes.

--- COMP260 Assignment 3/Assets/Scripts/Utils/LayerMaskExtensions.cs
+++ COMP260 Assignment 3/Assets/Scripts/Utils/LayerMaskExtensions.cs
@@ -1,7 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/**
+ * Extensions to Unity's LayerMask class
+ * 
+ */
+
 public static class LayerMaskExtensions  {
+
+	/**
+	 * Check if a particular gameobject is included in the layermask
+	 */
 
 	public static bool Contains(this LayerMask layerMask, GameObject gameObject) {
 		return (layerMask.value & (1 << gameObject.layer)) != 0;
