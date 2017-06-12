19a5ad9bbffd4d5dbf871d14ba4b0f1fba6da317
Aziza Magerramova <aziza@Azizas-MacBook-Air.local>
2017-06-12 23:16:48 +1000

    Track

---
+++ COMP260 Assignment 3/Assets/Scripts/kittyMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kittyMove : MonoBehaviour {
+
+	public float maxSpeed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 direction;
+		direction.x = Input.GetAxis("Vertical");
+		direction.y = Input.GetAxis("Vertical");
+
+		// scale by the maxSpeed parameter
+		Vector2 velocity = direction * maxSpeed;
+
+		// move the object
+		transform.Translate(velocity * Time.deltaTime);
+	}
+}
